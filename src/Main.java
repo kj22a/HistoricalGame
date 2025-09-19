234d975b28973fe3fc981e17e84a881573957c5e
kj22a <karujanjeya@gmail.com>
2025-09-20 00:02:08 +0100

    Adding 1 more archer and 1 mage to the army as a test.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Archer Archer1 = new Archer(100, 5);
+        Archer archer1 = new Archer(100, 5);
+        Archer archer2 = new Archer(150, 4);
+        Mage mage1 = new Mage(40, 12);
+
         Army army = new Army();
-        army.addWarrior(Archer1);
+        army.addWarrior(archer1);
+        army.addWarrior(archer2);
+        army.addWarrior(mage1);
         int armySize = army.armySize();
         System.out.println("The size of the army is: " + armySize);
-        }
     }
+}
